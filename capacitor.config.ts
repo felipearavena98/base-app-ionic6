905d9b1e36e74ef707e379815c1e9401d0240b1d
Felipe Aravena <felipe.aravena@egt.cl>
2023-04-20 15:03:36 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scannercordova',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
